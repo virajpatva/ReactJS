4d9453b7b2599adb2242d0fed4051c73ddd66637
viraj.patva <viraj.patva@smartsensesolutions.com>
2023-03-31 10:54:53 +0530

    list map used

--- FreeCodeCamp/my-app/src/index.js
+++ FreeCodeCamp/my-app/src/index.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css'
-
+const books = [
+  {
+    id: '1',
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/81N7FmJhbhL._AC_UL600_SR600,400_.jpg',
+    title: 'Life is Amazing',
+    author: 'Gopal Das'
+  }, {
+    id: '2',
+    img: 'https://images-eu.ssl-images-amazon.com/images/I/81N7FmJhbhL._AC_UL600_SR600,400_.jpg',
+    title: 'Life is Amazing 2 ',
+    author: 'Gopal Das'
+  }
+]
 const BookList = () => {
   return (
     <section className='bookList'>
-      <Book />
-      <Book />
+      {
+        books.map((book) => {
+          return (
+            <Book key={book.id} {...book} />
+          )
+        })
+      }
     </section>
   )
 }
 
-const Book = () => {
+const Book = ({ img, title, author }) => {
+
   return <article className='book'>
-    <img src="https://images-eu.ssl-images-amazon.com/images/I/81N7FmJhbhL._AC_UL600_SR600,400_.jpg" alt="" />
-    <h1>Life is Amazing</h1>
-    <h4 style={{ color: "green" }}>Gaur Gopal Das</h4>
+    <img src={img} alt="" />
+    <h1>{title}</h1>
+    <h4 style={{ color: "green" }}>{author}</h4>
+    
   </article>
 }
-
-ReactDOM.render(<BookList />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />)
